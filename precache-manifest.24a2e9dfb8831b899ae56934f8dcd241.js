58fbc94daf81380742206961b22a2016a49da0aa
charitarthchugh <charitarthchugh@users.noreply.github.com>
2023-01-13 02:02:21 +0000

    deploy: f967b10c971d17c18e04223085249e00fe03c948

---
+++ precache-manifest.24a2e9dfb8831b899ae56934f8dcd241.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87eeef7cfc72bcfdcb8a",
+    "url": "/LiquidPrep-Website/static/css/main.656967a4.chunk.css"
+  },
+  {
+    "revision": "87eeef7cfc72bcfdcb8a",
+    "url": "/LiquidPrep-Website/static/js/main.95d6ac6c.chunk.js"
+  },
+  {
+    "revision": "08908bfb82105fc71268",
+    "url": "/LiquidPrep-Website/static/js/runtime~main.f3cc2a2e.js"
+  },
+  {
+    "revision": "a69d7651ca9aa4e8ebc5",
+    "url": "/LiquidPrep-Website/static/js/2.8a683eed.chunk.js"
+  },
+  {
+    "revision": "dc5c2b7cb87f6a34d18083fe25e42e4c",
+    "url": "/LiquidPrep-Website/index.html"
+  }
+];
